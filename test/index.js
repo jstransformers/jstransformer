c2ceca676461a9852be843f8b4001151c583bf71
Rob Loach <robloach@gmail.com>
2015-05-25 13:53:29 -0400

    Fix if statement wrapper, and add async functions

--- test/index.js
+++ test/index.js
@@ -7,27 +7,50 @@ var test = require('testit');
 
 var transform = require('../');
 
-var input = fs.readFileSync(join(__dirname, 'input.txt')).toString();
+var inputFile = join(__dirname, 'input.txt');
+var input = fs.readFileSync(inputFile).toString();
 var options = require('./options');
 var locals = require('./locals');
-var expected = fs.readFileSync(join(__dirname, 'expected.txt')).toString();
+var expected = fs.readFileSync(join(__dirname, 'expected.txt')).toString().trim();
 
 function assertEqual(output, expected) {
   console.log('   Output:\t'   + JSON.stringify(output));
   console.log('   Expected:\t' + JSON.stringify(expected));
-  assert.equal(output, expected);
+  assert.equal(output.trim(), expected);
 }
 
-test(transform.name + '.render()', function () {
-  if (transform.render) {
+if (transform.render) {
+  test(transform.name + '.render()', function () {
     var output = transform.render(input, options, locals);
     assertEqual(output, expected);
-  }
-});
+  });
+}
 
-test(transform.name + '.compile()', function () {
-  if (transform.compile) {
+if (transform.compile) {
+  test(transform.name + '.compile()', function () {
     var output = transform.compile(input, options)(locals);
     assertEqual(output, expected);
-  }
-});
+  });
+}
+
+if (transform.renderAsync) {
+  test(transform.name + '.renderAsync()', function (done) {
+    transform.renderAsync(input, options, locals).then(function (output) {
+      assertEqual(output, expected);
+      done();
+    }, function (err) {
+      done(err);
+    }).done();
+  });
+}
+
+if (transform.renderFileAsync) {
+  test(transform.name + '.renderFileAsync()', function (done) {
+    transform.renderFileAsync(inputFile, options, locals).then(function (output) {
+      assertEqual(output, expected);
+      done();
+    }, function (err) {
+      done(err);
+    }).done();
+  });
+}
